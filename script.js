067a577c698c491b4e520e8ebf283ba7b2759445
Greg <greg19850@gmail.com>
2022-07-28 20:39:06 +0100

    Created function selectSeats, which highlight selected seats

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const countSpan = document.querySelector('.count');
 const priceSpan = document.querySelector('.price');
 
-let ticketPrice = movieSelection.value;
+let ticketPrice = +movieSelection.value;
+
+// AddEventListener to select not occupied seats
+
+const selectSeats = (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    countSelectedSeats();
+  }
+};
+
+container.addEventListener('click', selectSeats);
