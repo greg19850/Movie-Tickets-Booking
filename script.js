843745a9ebf6bde02d321421d39c5796e673714e
Greg <greg19850@gmail.com>
2022-11-12 08:13:46 +0000

    added functions setMovieData, populateSI. Updated function CountSelectedSeats

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 const container = document.querySelector('.container');
 const movieSelection = document.getElementById('movie');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-
 const countSpan = document.querySelector('.count');
 const priceSpan = document.querySelector('.price');
+
+populateUI();
 
 let ticketPrice = +movieSelection.value;
 
 //update of tickets price when selecting another movie
 
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
 const ticketPriceUpdate = (e) => {
   ticketPrice = e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   countSelectedSeats();
 };
 
 //add selected seats and ticket price to summary on bottom of page
 
 const countSelectedSeats = () => {
-  const selectedSeatsNumber = document.querySelectorAll(
-    '.row .seat.selected'
-  ).length;
+  const selectedSeats = [...document.querySelectorAll('.row .seat.selected')];
+
+  const selectedSeatsIndex = selectedSeats.map((seat) =>
+    [...seats].indexOf(seat)
+  );
+
+  localStorage.setItem('selectedSeatIndex', JSON.stringify(selectedSeatsIndex));
+
+  const selectedSeatsNumber = selectedSeats.length;
 
   countSpan.textContent = selectedSeatsNumber;
   priceSpan.textContent = selectedSeatsNumber * ticketPrice;
 
   movieSelection.addEventListener('change', ticketPriceUpdate);
 };
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeatIndex'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null)
+    movieSelection.selectedIndex = selectedMovieIndex;
+}
 
 // AddEventListener to select not occupied seats
 
@@ -41,3 +71,4 @@ const selectSeats = (e) => {
 };
 
 container.addEventListener('click', selectSeats);
+countSelectedSeats();
